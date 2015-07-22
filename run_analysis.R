1f49bccc4e854d1994e16b235671d63b9e1de8be
phamash <ashleypham19@gmail.com>
2015-07-22 13:19:19 -0400

    committing the run_analysis.R file

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-##extracting the data from the URL provided
+##extracting the data from the URL provided below
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
