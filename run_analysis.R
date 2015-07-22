4c681b39fbffe24e1022becdb33bebae4a26eee9
liem pham <liempham@PhamilyMac.townisp.com>
2015-07-22 13:01:03 -0400

    committing the run_analysis.R file for the course project

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+##extracting the data from the URL
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
