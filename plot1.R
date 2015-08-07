fc3ea70c972213dc07019d382499be10ab8bd452
phamash <ashleypham19@gmail.com>
2015-08-07 17:37:15 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,20 +1,20 @@
-## Pulling the dataset into R
-data <- read.csv("~/Desktop/Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+## Loading the dataset into R
+data_full <- read.csv("~/Desktop/data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
 ## Subsetting the data
-datasubset <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data)
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
 
 ## Converting dates
 datetime <- paste(as.Date(data$Date), data$Time)
-datasubset$Datetime <- as.POSIXct(datetime)
+data$Datetime <- as.POSIXct(datetime)
 
-## Plot 1
+## Plot1
 hist(data$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
 ## Saving to file
-dev.copy(png, file="plot1.png", height=480, width=480)
+png(file="plot1.png", height=480, width=480)
 dev.off()
